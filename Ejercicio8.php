31ee000322990c9a7c297a95829b48d062cc1cea
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-09-18 18:27:50 -0500

    Create Ejercicio8.php

---
+++ Ejercicio8.php
@@ -0,0 +1,10 @@
+<?php
+echo "<p>hello world!</p>";
+/*
+this is a multi-line comment block
+that spans over
+multiple lines
+*/
+echo "<p>i am learning php!</p>";
+echo"<p>this is muy first program!</p>";
+?>
