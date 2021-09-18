c98487c94d71124c80549b81fccc7697ae3b6d65
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-09-18 17:49:42 -0500

    Create Ejercicio16.php

---
+++ Ejercicio16.php
@@ -0,0 +1,15 @@
+<?php
+$num1 = 8;
+$num2 = 6;
+
+//addition
+echo $num1 + $num2; //14
+
+//subtraction
+echo $num1 - $num2; //2
+
+//multiplication
+echo $num1 * $num2; //48
+
+//division
+echo $num1 / $num2; //1.33333333333
