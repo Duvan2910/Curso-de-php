c34e9fb99d8655c07789134adba2e7fa2d85abc3
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-09-18 18:26:48 -0500

    Create Ejercicio50.php

---
+++ Ejercicio50.php
@@ -0,0 +1,53 @@
+<?php
+$value = "John";
+setcookie("user", $value, time() + (86400 * 30), '/'); 
+
+if(isset($_COOKIE['user'])) {
+  echo "Value is: ". $_COOKIE['user'];
+}
+//Outputs "Value is: John"
+?>
+
+$myfile = fopen("file.txt", "w");
+
+<?php
+$myfile = fopen("names.txt", "w");
+
+$txt = "John\n";
+fwrite($myfile, $txt);
+$txt = "David\n";
+fwrite($myfile, $txt);
+
+fclose($myfile);
+
+/* File contains:
+John
+David
+*/
+?>
+
+
+$myFile = "test.txt";
+$fh = fopen($myFile, 'a');
+fwrite($fh, "Some text");
+fclose($fh);
+
+<?php
+if(isset($_POST['text'])) {
+  $name = $_POST['text'];
+  $handle = fopen('names.txt', 'a');
+  fwrite($handle, $name."\n");
+  fclose($handle); 
+}
+?>
+<form method="post">
+  Name: <input type="text" name="text" /> 
+  <input type="submit" name="submit" />
+</form>
+
+class Person {
+  public $age; //property
+  public function speak() { //method
+    echo "Hi!"
+  }
+}
