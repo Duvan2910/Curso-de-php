d825a2fd93b474f6cc06667acd1c62ac40bcfdd7
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-09-18 17:51:51 -0500

    Create Ejercicio21.php

---
+++ Ejercicio21.php
@@ -0,0 +1,10 @@
+<?php
+$myarray[0] = "john";
+$myarray[1] = "<strong>php</strong>";
+$myarray[2] = 21;
+
+echo "$myarray[0] is $myarray[2] and knows $myarray[1]";
+
+// Outputrs "john is 21 and knows php"
+
+?>
